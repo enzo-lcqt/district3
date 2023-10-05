551b534130f0fd44ee3ed5ae465ad225a26eb2e3
[enzo] <[enzolocquet@gmail.com]>
2023-10-05 10:55:37 +0200

    dfs

--- db_connexion.php
+++ db_connexion.php
@@ -1,17 +1,18 @@
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
-$servername = "amorce.org";
-$username = "locquet";
-$password = "momo3006";
-$dbname = "locquet";
-$port = "3306";
 
 try {
-    $conn = new PDO("mysql:host=$servername;port=$port;dbname=$dbname;charset=utf8", $username, $password);
-
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db = new PDO('mysql:host=localhost;dbname=locquet', 'locquet', 'momo3006');
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("La connexion à la base de données a échoué : " . $e->getMessage());
+    echo 'Erreur de connexion : ' . $e->getMessage();
+    exit; 
 }
+
+$stmt = $db->prepare("SELECT * FROM locquet");
+$stmt->execute();
+$result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+
 ?>
